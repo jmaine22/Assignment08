63e06fcbbad8bdbdb336937ce9bec09d3599f046
Jmaine22 <jmaine22@gmail.com>
2021-04-14 22:06:28 -0700

    Assignement08

--- js/arrays-practice.js
+++ js/arrays-practice.js
@@ -2,20 +2,35 @@
 let favMovies = ['Love Jones', 'Love & Basketball', 'The Matrix', 'Ready Player One', 'A Low Down Dirty Shame'];
 console.log(favMovies[1]);
 //STEP 2
-let movies = new Array(5);
-movies[0] = favMovies[0];
-movies[1] = favMovies[1];
-movies[2] = favMovies[2];
-movies[3] = favMovies[3];
-movies[4] = favMovies[4];
-console.log(movies[0]);
+// let movies = new Array(5);
+// movies[0] = favMovies[0];
+// movies[1] = favMovies[1];
+// movies[2] = favMovies[2];
+// movies[3] = favMovies[3];
+// movies[4] = favMovies[4];
+// console.log(movies[0]);
 //STEP 3
-movies.splice(2, 0, 'Spiderman');
-console.log(movies.length);
-console.log(movies);
+// movies.splice(2, 0, 'Spiderman');
+// console.log(movies.length);
+// console.log(movies);
 //STEP 4
-
+// let movies = []
+// movies[0] = "Total Recall";
+// movies[1] = "Ghost in the Shell";
+// movies[2] = "House Party";
+// movies[3] = "krush Groove";
+// movies[4] = "The Godfather";
+// movies.shift();
+// console.log(movies);
 //STEP 5
+let movies = [];
+movies[0] = "Resident Evil";
+movies[1] = "Ali";
+movies[2] = "Sister Act";
+movies[3] = "Rocky";
+movies[4] = "DC Cab";
+movies[5] = "Problem Child";
+movies[6] = "Friday";
 
 //STEP 6
 
